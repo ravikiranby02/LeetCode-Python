927a8b83ebb4fb11967624484b80a784bf31daa3
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-03-03 00:21:34 +0530

    Add files via upload

---
+++ 001-100/12. Transform Array by Parity.py
@@ -0,0 +1,12 @@
+"""Transform Array by Parity"""
+
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in nums:
+            if i%2==0:
+                result.append(0)
+            else:
+                result.append(1)
+        result.sort()
+        return result
