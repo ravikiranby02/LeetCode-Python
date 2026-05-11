453862f3b4ffc2ffd622ec5e0e161f9967507efa
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-05-11 07:21:59 +0530

    Add files via upload

---
+++ 001-100/080. Minimum Number Game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        result = []
+
+        for i in range(0, len(nums), 2):
+            result.append(nums[i+1])
+            result.append(nums[i])
+        return result
