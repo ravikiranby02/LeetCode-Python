8d7f97a5516dfa7fe862b79727f27129ae060718
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-05-19 18:56:24 +0530

    Add files via upload

---
+++ 001-100/088. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original, cloned, target):
+        if not original:
+            return None
+        
+        if original == target:
+            return cloned
+        
+        left = self.getTargetCopy(
+            original.left,
+            cloned.left,
+            target
+        )
+        
+        if left:
+            return left
+        
+        return self.getTargetCopy(
+            original.right,
+            cloned.right,
+            target
+        )
