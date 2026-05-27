fd65454b59e104c6ef74c4447128a982c38daee2
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-05-27 07:16:00 +0530

    Add files via upload

---
+++ 001-100/096. Count Partitions with Even Sum Difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countPartitions(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+
+        if total_sum % 2 != 0:
+            return 0
+
+        return len(nums) - 1
