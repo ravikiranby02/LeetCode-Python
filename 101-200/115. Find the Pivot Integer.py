639931b55c191a0e7fb5795d18138e081b872633
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-06-15 09:11:16 +0530

    Add files via upload

---
+++ 101-200/115. Find the Pivot Integer.py
@@ -0,0 +1,8 @@
+from math import isqrt
+
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        total = n * (n + 1) // 2
+        x = isqrt(total)
+
+        return x if x * x == total else -1
